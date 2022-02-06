20494b31a0325302d4e76312112559be2607469c
thisisartorius <artur.poe@gmail.com>
2022-02-05 20:09:56 -0500

    Api

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+const URL = 'https://api.nomoreparties.co';
+
+const SHORT_MOVIE_DURATION = 40;
+
+const CARDS_TO_ADD_FOR_MIN_WW = 2;
+
+const CARDS_TO_ADD_FOR_MAX_WW = 3;
+
+const CARDS_COUNT_FOR_MIN_WW = 5;
+
+const CARDS_COUNT_FOR_MEDIUM_WW = 8;
+
+const CARDS_COUNT_FOR_MAX_WW = 12;
+
+export {
+    URL,
+    SHORT_MOVIE_DURATION,
+    CARDS_TO_ADD_FOR_MIN_WW,
+    CARDS_TO_ADD_FOR_MAX_WW,
+    CARDS_COUNT_FOR_MIN_WW,
+    CARDS_COUNT_FOR_MEDIUM_WW,
+    CARDS_COUNT_FOR_MAX_WW,
+};
---
+++ src/utils/options.js
@@ -0,0 +1,10 @@
+const reqOptions = {
+    baseUrl: 'https://api.superdiploma.nomoredomains.rocks',
+    beatfilmUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
+};
+
+export default reqOptions;
